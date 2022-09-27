36bbe6955b5625acfadfc7811d6811a1765099e3
RamonKick <ramon.francoo@hotmail.com>
2022-09-27 11:18:41 -0300

    Teste

---
+++ FixaxionException/Program.cs
@@ -0,0 +1,38 @@
+﻿using FixationException.Entities;
+using FixationException.Entities.Exception;
+using System;
+using System.Globalization;
+
+namespace FixationException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial Balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture) ;
+
+                Account account = new Account(number, holder, balance, withdrawLimit);
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account.Withdraw(amount);
+            }
+
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
